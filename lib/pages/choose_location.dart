69fe8462d43c726511e110fe4cb54d786d2a47db
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2022-01-27 18:46:20 +0600

    Asynchronous Code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
@@ -8,31 +10,36 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  void getData() async {
+    // simulate network request for name
+    String userName = await Future.delayed(Duration(seconds: 3), () {
+      return 'Mahfuj Ahmed Jim';
+    });
+
+    // simulate network request for bio
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'Android Developer';
+    });
+
+    print('$userName - $bio');
+  }
 
   @override
   void initState() {
     super.initState();
-    print('Init state');
+    getData();
+    print('Hey there');
   }
 
   @override
   Widget build(BuildContext context) {
-    print('$counter');
     return Scaffold(
-        backgroundColor: Colors.grey[800],
-        appBar: AppBar(
-          backgroundColor: Colors.grey[900],
-          title: Text('Chooce Location'),
-          elevation: 0,
-        ),
-        body: RaisedButton(
-          onPressed: () {
-            setState(() {
-              counter++;
-            });
-          },
-          child: Text('Counter is $counter'),
-        ));
+      backgroundColor: Colors.grey[800],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: Text('Chooce Location'),
+        elevation: 0,
+      ),
+    );
   }
 }
