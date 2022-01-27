24ecb1b3aa0f704472c3839f9ba8b11fc7e25fbc
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2022-01-27 16:24:46 +0600

    Maps & Routing

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: Text('Chooce Location'),
+        elevation: 0,
+      ),
+      body: Text('Choose Location'),
+    );
+  }
+}
